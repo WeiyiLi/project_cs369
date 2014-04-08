517b9fa93eebf2cf80db4c080197cf7e93f08ca8
Weiyi Li <liweiyi2009@gmail.com>
2014-04-08 11:01:45 -0500

    Add rspec tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Media Tracker') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+  end
+end
