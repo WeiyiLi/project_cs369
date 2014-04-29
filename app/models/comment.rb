eada4e329fa069708701a74479f341a6edb49f43
Eric Freitas <eric.x6@gmail.com>
2014-04-29 13:08:10 -0500

    Creates tests and users and comments models interactions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+	belongs_to :user
+	default_scope -> { order('created_at DESC')}
+	validates :content, presence: true, length: {maximum: 350}
+	validates :user_id, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryGirl.create(:user) }
+  before { @comment = user.comments.build(content: "Loremipsum") }
+
+  subject { @comment }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @comment.user_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do
+    before { @comment.content = "a" * 351 }
+    it { should_not be_valid }
+  end
 end
